ae0310ac0790a1804a16a450e4febed47f18c2b6
mkJoo <mkjoo0909@gmail.com>
2021-03-18 22:09:38 +0900

    1. pod 구글 설정 추가 2. app 실행 로딩 화면 3. login 화면 추가 4. 구글로 로그인 진행중 5. 채팅 리스트 스토리보드 추가

--- IOSChatApp/LoginViewController.swift
+++ IOSChatApp/LoginViewController.swift
@@ -7,12 +7,73 @@
 
 import Foundation
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //GIDSignIn.sharedInstance().presentingViewController = self
+        self.setLoginView()
         // Do any additional setup after loading the view.
     }
 
+}
+
+private extension LoginViewController {
+    
+    @IBAction func googleSignIn(_ sender: Any) {
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+    }
+    
+    func setLoginView() {
+        
+        self.loginButton.addTarget(self, action: #selector(loginEvent), for: .touchUpInside)
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user != nil {
+                guard let chatListView = self.storyboard?.instantiateViewController(withIdentifier: "ChatListViewController") as? ChatViewController else {return}
+                self.present(chatListView, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!,
+      withError error: NSError!) {
+        if (error == nil) {
+          // Perform any operations on signed in user here.
+          // ...
+        } else {
+          print("\(error.localizedDescription)")
+        }
+    }
+}
+
+private extension LoginViewController {
+    @objc func loginEvent() {
+        
+        guard let email = self.emailTextField.text,
+              let password = self.passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, err) in
+            
+            if err != nil {
+                let alert = UIAlertController(title: "ERROR", message: err.debugDescription, preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                if user != nil {
+                    guard let chatListView = self.storyboard?.instantiateViewController(withIdentifier: "ChatListViewController") as? ChatViewController else { return }
+                    self.present(chatListView, animated: true, completion: nil)
+                }
+            }
+                
+        }
+    }
 }
--- IOSChatApp/ViewController.swift
+++ IOSChatApp/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    var box = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setCenterImage()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.callLoginView()
     }
 
 
 }
 
+private extension ViewController {
+    
+    func setCenterImage() {
+        self.view.addSubview(self.box)
+        self.box.frame = CGRect.init(x: self.view.frame.width/2, y: self.view.frame.height/2, width: 100, height: 100)
+        self.box.snp.makeConstraints{ (make) in
+            make.center.equalTo(self.view)
+            make.width.height.equalTo(100)
+        }
+
+        self.box.image = #imageLiteral(resourceName: "talkIcon")
+        //self.view.backgroundColor = UIColor.black
+        
+        
+        
+    }
+    
+    func callLoginView() {
+        print("aaa")
+        guard let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController
+        else { return print("aaas") }
+        
+        self.present(loginVC, animated: false, completion: nil)
+    }
+
+}
